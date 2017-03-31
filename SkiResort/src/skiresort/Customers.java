d0d4594b58744bbf4958a485b58fe6c8f361aeaf
Judgey19XX <liamjudge2012@gmail.com>
2017-04-01 00:19:30 +0100

    Added Data Persistence Layer
    1. Added data base connectivity
    2. Added Proposed changes, i.e. the customer ID is now automatically generated

---
+++ SkiResort/src/skiresort/Customers.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skiresort;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author liamjudge
+ */
+@Entity
+@Table(name = "CUSTOMERS")
+@NamedQueries({
+    @NamedQuery(name = "Customers.findAll", query = "SELECT c FROM Customers c")
+    , @NamedQuery(name = "Customers.findById", query = "SELECT c FROM Customers c WHERE c.id = :id")
+    , @NamedQuery(name = "Customers.findByFirstname", query = "SELECT c FROM Customers c WHERE c.firstname = :firstname")
+    , @NamedQuery(name = "Customers.findByLastname", query = "SELECT c FROM Customers c WHERE c.lastname = :lastname")
+    , @NamedQuery(name = "Customers.findByAddress", query = "SELECT c FROM Customers c WHERE c.address = :address")
+    , @NamedQuery(name = "Customers.findByPhoneno", query = "SELECT c FROM Customers c WHERE c.phoneno = :phoneno")})
+public class Customers implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+    @Column(name = "LASTNAME")
+    private String lastname;
+    @Column(name = "ADDRESS")
+    private String address;
+    @Column(name = "PHONENO")
+    private String phoneno;
+
+    public Customers() {
+    }
+
+    public Customers(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        Integer oldId = this.id;
+        this.id = id;
+        changeSupport.firePropertyChange("id", oldId, id);
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        String oldFirstname = this.firstname;
+        this.firstname = firstname;
+        changeSupport.firePropertyChange("firstname", oldFirstname, firstname);
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        String oldLastname = this.lastname;
+        this.lastname = lastname;
+        changeSupport.firePropertyChange("lastname", oldLastname, lastname);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        String oldAddress = this.address;
+        this.address = address;
+        changeSupport.firePropertyChange("address", oldAddress, address);
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        String oldPhoneno = this.phoneno;
+        this.phoneno = phoneno;
+        changeSupport.firePropertyChange("phoneno", oldPhoneno, phoneno);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customers)) {
+            return false;
+        }
+        Customers other = (Customers) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "skiresort.Customers[ id=" + id + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
