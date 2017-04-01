51dc1bc4e9c815ba9b83ea2a46d0de31836e76fd
Holmesy221 <holmesy221@gmail.com>
2017-04-01 21:31:27 +0100

    CheckInAndAddUser

---
+++ SkiResort/build/generated-sources/ap-source-output/skiresort/Customers_.java
@@ -0,0 +1,17 @@
+package skiresort;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-01T21:22:45")
+@StaticMetamodel(Customers.class)
+public class Customers_ { 
+
+    public static volatile SingularAttribute<Customers, String> firstname;
+    public static volatile SingularAttribute<Customers, String> address;
+    public static volatile SingularAttribute<Customers, Integer> id;
+    public static volatile SingularAttribute<Customers, String> phoneno;
+    public static volatile SingularAttribute<Customers, String> lastname;
+
+}
