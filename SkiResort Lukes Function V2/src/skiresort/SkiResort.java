a481558cc239aa5739e4eec308163599f9720a90
jamesl7 <jamesl7@uni.coventry.ac.uk>
2017-04-02 19:52:52 +0100

    Add files via upload

---
+++ SkiResort Lukes Function V2/src/skiresort/SkiResort.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skiresort;
+
+/**
+ *
+ * @author liamjudge
+ */
+public class SkiResort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AddCustomer ac = new AddCustomer();
+        ac.setVisible(true);
+    }
+    
+}
